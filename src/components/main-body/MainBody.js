7f3ee7f6d18510709a795b61a7d933e19d66c641
Sabina Maharjan <saveena1414@gmail.com>
2018-09-11 21:32:17 +0545

    update classname

--- src/components/main-body/MainBody.js
+++ src/components/main-body/MainBody.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import cn from 'classnames';
 
 import styles from './MainBody.scss';
 
-const MainBody = ({ children }) => <div className={cn('row', styles.mainBody)}>{children}</div>;
+const MainBody = ({ children }) => <div className={styles.mainBody}>{children}</div>;
 
 export default MainBody;
--- src/components/section/QueueSection.js
+++ src/components/section/QueueSection.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import styles from './Section.scss';
 
 const QueueSection = ({ children }) => (
-  <div className={styles.QueueSection}>
+  <div className={styles.queueSection}>
     <h1>What&apos;s Next</h1>
     <div className={styles.listSection}>{children}</div>
   </div>
